d1fef614ac684f65de0969c2299d156c8515f5c4
kisho <kisho.dai@mi-me.com>
2017-06-09 11:08:22 +0800

    optimize StartMain

--- src/main/java/cn/memedai/orientdb/sns/realtime/batch/BatchService.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/batch/BatchService.groovy
@@ -1,9 +1,9 @@
 package cn.memedai.orientdb.sns.realtime.batch
 
 import cn.memedai.orientdb.sns.realtime.common.OrientDb
+import groovy.sql.Sql
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.slf4j.LoggerFactory
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
 import javax.annotation.Resource
@@ -20,11 +20,14 @@ class BatchService {
     private OrientDb orientDb
 
     @Resource
-    private JdbcTemplate jdbcTemplate
+    private Sql sql
 
     void process(ConsumerRecords records) {
         LOG.debug(orientDb.getDb().toString())
-        LOG.debug(jdbcTemplate.toString())
+        sql.eachRow('select count(1) as num from network.apply_info', {
+            row ->
+                LOG.info("" + row.num)
+        })
         //TODO
     }
 
