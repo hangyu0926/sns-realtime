cee69c820d542abcfd1172900fcdacb0d59f5981
ti zhou <ti.zhou@nan-mi.cn>
2017-06-14 10:46:45 +0800

    MemberToOrientDBServiceImpl  要加入判空

--- src/main/java/cn/memedai/orientdb/sns/realtime/service/impl/toorientdb/MemberToOrientDBServiceImpl.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/service/impl/toorientdb/MemberToOrientDBServiceImpl.groovy
@@ -6,6 +6,7 @@ import cn.memedai.orientdb.sns.realtime.cache.MemberCache
 import cn.memedai.orientdb.sns.realtime.sql.OrientSql
 import cn.memedai.orientdb.sns.realtime.service.RealTimeService
 import cn.memedai.orientdb.sns.realtime.util.OrientSqlUtil
+import org.apache.commons.lang.StringUtils
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -31,7 +32,12 @@ class MemberToOrientDBServiceImpl implements RealTimeService {
     private String updateMemberSql = 'update Member set memberId=?,name=?,idNo=?,province=?,city=?,phone=?,isBlack=false,isOverdue=false upsert return after where memberId=?'
 
     void process(List<Map<String, Object>> dataList) {
-        for (def i = 0; i < dataList.size(); i++){
+        if (dataList == null) {
+            return
+        }
+
+        int size = dataList.size()
+        for (def i = 0; i < size; i++) {
             Map<String, Object> memberMap = dataList.get(i)
 
             String idNo = memberMap.ID_NO
@@ -39,17 +45,22 @@ class MemberToOrientDBServiceImpl implements RealTimeService {
             String city = memberMap.CITY
 
             if (idNo != null && idNo.trim().length() > 6) {
-                Map<String, String> idAddress = idCardCache.get(idNo.substring(0, 6)).value;
+                Map<String, String> idAddress = null
+                CacheEntry idCardCacheEntry = idCardCache.get(idNo.substring(0, 6))
+                if (idCardCacheEntry != null) {
+                    idAddress = idCardCacheEntry.value
+                }
                 if (idAddress != null) {
                     province = idAddress.PROVINCE
                     city = idAddress.city
                 }
             }
 
-            String memberRid = OrientSqlUtil.getRid(orientSql.execute(updateMemberSql, memberMap.MEMBER_ID,memberMap.NAME,idNo,province,city,
-                    memberMap.MOBILE_NO,memberMap.MEMBER_ID))
-
-            memberCache.put(new CacheEntry(memberMap.MEMBER_ID, memberRid))
+            String memberRid = OrientSqlUtil.getRid(orientSql.execute(updateMemberSql, memberMap.MEMBER_ID, memberMap.NAME, idNo, province, city,
+                    memberMap.MOBILE_NO, memberMap.MEMBER_ID))
+            if (StringUtils.isNotBlank(memberRid)) {
+                memberCache.put(new CacheEntry(memberMap.MEMBER_ID, memberRid))
+            }
         }
     }
 
