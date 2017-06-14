894febbc93cf07a3b685318f2afb0622aa697936
ti zhou <ti.zhou@nan-mi.cn>
2017-06-14 10:26:16 +0800

    ApplyToOrientDBServiceImpl 从CacheEntry取值要加null判断，以及取出的value加入blank判断

--- src/main/java/cn/memedai/orientdb/sns/realtime/service/impl/toorientdb/ApplyToOrientDBServiceImpl.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/service/impl/toorientdb/ApplyToOrientDBServiceImpl.groovy
@@ -4,6 +4,7 @@ import cn.memedai.orientdb.sns.realtime.cache.*
 import cn.memedai.orientdb.sns.realtime.service.RealTimeService
 import cn.memedai.orientdb.sns.realtime.sql.OrientSql
 import cn.memedai.orientdb.sns.realtime.util.OrientSqlUtil
+import org.apache.commons.lang.StringUtils
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -40,27 +41,66 @@ class ApplyToOrientDBServiceImpl implements RealTimeService {
     void process(List<Map<String, Object>> dataList) {
         Map<String, Object> applyMap = dataList.get(0)
 
-        String memberRid = memberCache.get(applyMap.member_id).value
+        String memberRid = null
+        CacheEntry memberCacheEntry = memberCache.get(applyMap.member_id)
+        if (memberCacheEntry != null) {
+            memberRid = memberCacheEntry.value
+        }
 
-        String phoneRid = phoneCache.get(applyMap.cellphone).value
+        String phoneRid = null
+        CacheEntry phoneCacheEntry = phoneCache.get(applyMap.cellphone)
+        if (phoneCacheEntry != null) {
+            memberRid = phoneCacheEntry.value
+        }
 
         String applyRid = OrientSqlUtil.getRid(orientSql.execute(updateApplySql, applyMap.apply_no, getStatus(applyMap.apply_status), applyMap.apply_status, applyMap.created_datetime, applyMap.apply_no))
+        if (StringUtils.isBlank(applyRid)) {
+            return
+        }
         applyCache.put(new CacheEntry(applyMap.apply_no, applyRid))
 
-        orientSql.createEdge('HasPhone', memberRid, phoneRid)
-        orientSql.createEdge('MemberHasApply', memberRid, applyRid)
-        orientSql.createEdge('PhoneHasApply', phoneRid, applyRid)
+        if (StringUtils.isNotBlank(memberRid) && StringUtils.isNotBlank(phoneRid)) {
+            orientSql.createEdge('HasPhone', memberRid, phoneRid)
+        }
+
+        if (StringUtils.isNotBlank(memberRid)) {
+            orientSql.createEdge('MemberHasApply', memberRid, applyRid)
+        }
+
+        if (StringUtils.isNotBlank(phoneRid)) {
+            orientSql.createEdge('PhoneHasApply', phoneRid, applyRid)
+        }
 
         if (applyMap.order_no != null) {
-            String orderRid = orderCache.get(applyMap.order_no).value
-            orientSql.createEdge('MemberHasOrder', memberRid, orderRid)
-            orientSql.createEdge('PhoneHasOrder', phoneRid, orderRid)
-            orientSql.createEdge('ApplyHasOrder', applyRid, orderRid)
+            String orderRid = null
+            CacheEntry orderCacheEntry = orderCache.get(applyMap.order_no)
+            if (orderCacheEntry != null) {
+                orderRid = orderCacheEntry.value
+            }
+
+            if (StringUtils.isNotBlank(orderRid)) {
+                if (StringUtils.isNotBlank(memberRid)) {
+                    orientSql.createEdge('MemberHasOrder', memberRid, orderRid)
+                }
+
+                if (StringUtils.isNotBlank(phoneRid)) {
+                    orientSql.createEdge('PhoneHasOrder', phoneRid, orderRid)
+                }
+
+                orientSql.createEdge('ApplyHasOrder', applyRid, orderRid)
+            }
         }
 
         if (applyMap.store_id != null) {
-            String storeRid = storeCache.get(applyMap.store_id).value
-            orientSql.createEdge('ApplyHasStore', applyRid, storeRid)
+            String storeRid = null
+            CacheEntry storeCacheEntry = storeCache.get(applyMap.store_id)
+            if (storeCacheEntry != null) {
+                storeRid = storeCacheEntry.value
+            }
+
+            if (StringUtils.isNotBlank(storeRid)) {
+                orientSql.createEdge('ApplyHasStore', applyRid, storeRid)
+            }
         }
     }
 
