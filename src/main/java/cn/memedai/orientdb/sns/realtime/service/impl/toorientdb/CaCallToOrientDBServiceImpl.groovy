dc9f8228460e576d6c713d72d424d4e3c82048d7
kisho <kisho.dai@mi-me.com>
2017-06-20 17:03:07 +0800

    添加jdbcTemplate

--- src/main/java/cn/memedai/orientdb/sns/realtime/service/impl/toorientdb/CaCallToOrientDBServiceImpl.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/service/impl/toorientdb/CaCallToOrientDBServiceImpl.groovy
@@ -1,20 +1,15 @@
 package cn.memedai.orientdb.sns.realtime.service.impl.toorientdb
 
-import cn.memedai.orientdb.sns.realtime.cache.ApplyCache
-import cn.memedai.orientdb.sns.realtime.cache.ApplyHasDoCache
-import cn.memedai.orientdb.sns.realtime.cache.ApplyRidPhoneRidCache
-import cn.memedai.orientdb.sns.realtime.cache.CacheEntry
-import cn.memedai.orientdb.sns.realtime.cache.PhoneCache
-
-import cn.memedai.orientdb.sns.realtime.sql.OrientSql
+import cn.memedai.orientdb.sns.realtime.cache.*
 import cn.memedai.orientdb.sns.realtime.service.RealTimeService
+import cn.memedai.orientdb.sns.realtime.sql.OrientSql
 import com.orientechnologies.orient.core.id.ORecordId
 import com.orientechnologies.orient.core.record.impl.ODocument
 import com.orientechnologies.orient.core.sql.query.OResultSet
-import groovy.sql.Sql
 import org.apache.commons.collections.CollectionUtils
 import org.apache.commons.lang.StringUtils
 import org.slf4j.LoggerFactory
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
 import javax.annotation.Resource
@@ -44,7 +39,7 @@ class CaCallToOrientDBServiceImpl implements RealTimeService {
     private OrientSql orientSql
 
     @Resource
-    private Sql sql
+    private JdbcTemplate jdbcTemplate
 
     private String checkEdgeSql = 'select from (select expand(out_{0}) from {1}) where in = {2}'
 
@@ -64,7 +59,7 @@ class CaCallToOrientDBServiceImpl implements RealTimeService {
 
         String appNo = (String) callToMap.APPL_NO
 
-        LOG.info("appNo is {}",appNo)
+        LOG.info("appNo is {}", appNo)
         if (StringUtils.isBlank(appNo)) {
             return
         }
@@ -91,49 +86,51 @@ class CaCallToOrientDBServiceImpl implements RealTimeService {
             return
         }
 
-        LOG.info("fromPhoneRid is {}",fromPhoneRid)
+        LOG.info("fromPhoneRid is {}", fromPhoneRid)
 
         String toPhoneRid = null
 
-       /* sql.query('select APPL_NO,PHONE_NO,CALL_CNT,CALL_LEN,CALL_IN_CNT,CALL_OUT_CNT,CREATE_TIME from network.ca_bur_operator_contact where PHONE_NO is not null and APPL_NO = "'+ appNo+'"') {
-            rs ->
-                while (rs.next()) {
-                       String toPhone = rs.getString("PHONE_NO")
-                    int callCnt = rs.getInt("CALL_CNT")
-                    int callLen = rs.getInt("CALL_LEN")
-                    int callInCnt = rs.getInt("CALL_IN_CNT")
-                    int callOutCnt = rs.getInt("CALL_OUT_CNT")
-                    String createTime = rs.getString("CREATE_TIME")
-                */
-          sql.eachRow('select APPL_NO,PHONE_NO,CALL_CNT,CALL_LEN,CALL_IN_CNT,CALL_OUT_CNT,CREATE_TIME from network.ca_bur_operator_contact where PHONE_NO is not null and APPL_NO = "'+ appNo+'"'){
-                row ->
-                    String toPhone = row.PHONE_NO
-                    int callCnt = row.CALL_CNT
-                    int callLen = row.CALL_LEN
-                    int callInCnt = row.CALL_IN_CNT
-                    int callOutCnt = row.CALL_OUT_CNT
-                    String createTime = row.CREATE_TIME
+        /* sql.query('select APPL_NO,PHONE_NO,CALL_CNT,CALL_LEN,CALL_IN_CNT,CALL_OUT_CNT,CREATE_TIME from network.ca_bur_operator_contact where PHONE_NO is not null and APPL_NO = "'+ appNo+'"') {
+             rs ->
+                 while (rs.next()) {
+                        String toPhone = rs.getString("PHONE_NO")
+                     int callCnt = rs.getInt("CALL_CNT")
+                     int callLen = rs.getInt("CALL_LEN")
+                     int callInCnt = rs.getInt("CALL_IN_CNT")
+                     int callOutCnt = rs.getInt("CALL_OUT_CNT")
+                     String createTime = rs.getString("CREATE_TIME")
+                 */
 
-                    CacheEntry phoneCacheEntry = phoneCache.get(toPhone)
-                    if (phoneCacheEntry != null) {
-                        toPhoneRid = phoneCacheEntry.value
-                    }
-                    if (StringUtils.isBlank(toPhoneRid)) {
-                        return
-                    }
+        jdbcTemplate.queryForList('select APPL_NO,PHONE_NO,CALL_CNT,CALL_LEN,CALL_IN_CNT,CALL_OUT_CNT,CREATE_TIME from network.ca_bur_operator_contact where PHONE_NO is not null and APPL_NO =?',
+                appNo).each {
+            def row = it
+            String toPhone = row.PHONE_NO
+            int callCnt = row.CALL_CNT
+            int callLen = row.CALL_LEN
+            int callInCnt = row.CALL_IN_CNT
+            int callOutCnt = row.CALL_OUT_CNT
+            String createTime = row.CREATE_TIME
+            LOG.info(toPhone)
+            CacheEntry phoneCacheEntry = phoneCache.get(toPhone)
+            if (phoneCacheEntry != null) {
+                toPhoneRid = phoneCacheEntry.value
+            }
+            if (StringUtils.isBlank(toPhoneRid)) {
+                return
+            }
 
-                    def args = [callCnt, callLen, callInCnt, callOutCnt, createTime] as Object[]
+            def args = [callCnt, callLen, callInCnt, callOutCnt, createTime] as Object[]
 
-                    OResultSet ocrs = orientSql.execute(MessageFormat.format(checkEdgeSql, "CallTo", fromPhoneRid, toPhoneRid))
-                    if (CollectionUtils.isEmpty(ocrs)) {
-                        orientSql.execute(MessageFormat.format(createEdgeSql, fromPhoneRid, toPhoneRid), args)
-                    } else {
-                        ODocument doc = (ODocument) ocrs.get(0)
-                        ORecordId oRecordId = doc.field("@rid")
-                        orientSql.execute(MessageFormat.format(updateEdgeSql, oRecordId.getIdentity().toString()),args)
-                    }
-                }
-       // }
+            OResultSet ocrs = orientSql.execute(MessageFormat.format(checkEdgeSql, "CallTo", fromPhoneRid, toPhoneRid))
+            if (CollectionUtils.isEmpty(ocrs)) {
+                orientSql.execute(MessageFormat.format(createEdgeSql, fromPhoneRid, toPhoneRid), args)
+            } else {
+                ODocument doc = (ODocument) ocrs.get(0)
+                ORecordId oRecordId = doc.field("@rid")
+                orientSql.execute(MessageFormat.format(updateEdgeSql, oRecordId.getIdentity().toString()), args)
+            }
+        }
+        // }
 
         //写入缓存
         applyHasDoCache.put(new CacheEntry(appNo, true))
