c4f9cc972a93bcbce73321c92f975fdd9ae70ea0
yu.hang <yu.hang@nan-mi.cn>
2017-06-21 15:07:29 +0800

    添加(现金贷)mysql到orientDb部分创建逻辑

--- src/main/java/cn/memedai/orientdb/sns/realtime/util/DateUtils.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/util/DateUtils.groovy
@@ -13,7 +13,7 @@ import java.text.SimpleDateFormat
 class DateUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(DateUtils.class);
 
-    public static String getStartDatetime(String startDatetime, int i) {
+     static String getStartDatetime(String startDatetime, int i) {
         if (StringUtils.isBlank(startDatetime)) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd")
             Date date = new Date(Calendar.getInstance().getTimeInMillis() - i * 3600 * 24 * 1000)
@@ -23,7 +23,7 @@ class DateUtils {
     }
 
 
-    public static Date StringToDate(String createDate) {
+     static Date StringToDate(String createDate) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         Date date = null
         try {
@@ -37,14 +37,14 @@ class DateUtils {
     // date类型转换为String类型
     // formatType格式为yyyy-MM-dd HH:mm:ss//yyyy年MM月dd日 HH时mm分ss秒
     // data Date类型的时间
-    public static String dateToString(Date data, String formatType) {
+     static String dateToString(Date data, String formatType) {
         return new SimpleDateFormat(formatType).format(data)
     }
 
     // long类型转换为String类型
     // currentTime要转换的long类型的时间
     // formatType要转换的string类型的时间格式
-    public static String longToString(long currentTime, String formatType)
+     static String longToString(long currentTime, String formatType)
             throws ParseException {
         Date date = longToDate(currentTime, formatType);// long类型转成Date类型
         String strTime = dateToString(date, formatType);// date类型转成String
@@ -55,7 +55,7 @@ class DateUtils {
     // strTime要转换的string类型的时间，formatType要转换的格式yyyy-MM-dd HH:mm:ss//yyyy年MM月dd日
     // HH时mm分ss秒，
     // strTime的时间格式必须要与formatType的时间格式相同
-    public static Date stringToDate(String strTime, String formatType)
+     static Date stringToDate(String strTime, String formatType)
             throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat(formatType)
         Date date = null
@@ -66,7 +66,7 @@ class DateUtils {
     // long转换为Date类型
     // currentTime要转换的long类型的时间
     // formatType要转换的时间格式yyyy-MM-dd HH:mm:ss//yyyy年MM月dd日 HH时mm分ss秒
-    public static Date longToDate(long currentTime, String formatType)
+     static Date longToDate(long currentTime, String formatType)
             throws ParseException {
         Date dateOld = new Date(currentTime) // 根据long类型的毫秒数生命一个date类型的时间
         String sDateTime = dateToString(dateOld, formatType) // 把date类型的时间转换为string
@@ -78,7 +78,7 @@ class DateUtils {
     // strTime要转换的String类型的时间
     // formatType时间格式
     // strTime的时间格式和formatType的时间格式必须相同
-    public static long stringToLong(String strTime, String formatType)
+     static long stringToLong(String strTime, String formatType)
             throws ParseException {
         Date date = stringToDate(strTime, formatType) // String类型转成date类型
         if (date == null) {
@@ -91,7 +91,7 @@ class DateUtils {
 
     // date类型转换为long类型
     // date要转换的date类型的时间
-    public static long dateToLong(Date date) {
+     static long dateToLong(Date date) {
         return date.getTime()
     }
 }
