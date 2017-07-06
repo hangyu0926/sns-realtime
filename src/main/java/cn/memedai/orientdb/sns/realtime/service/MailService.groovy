4f5c51cc98bf42f58c38b824a948c12622815426
yu.hang <yu.hang@nan-mi.cn>
2017-07-06 18:01:09 +0800

    提交测试的配置

--- src/main/java/cn/memedai/orientdb/sns/realtime/service/MailService.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/service/MailService.groovy
@@ -1,7 +1,6 @@
 package cn.memedai.orientdb.sns.realtime.service
 
 import cn.memedai.orientdb.sns.realtime.util.MailUtils
-import org.apache.commons.codec.binary.Base64
 import org.apache.commons.lang.StringUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
