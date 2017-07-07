3dfd8d02042aa24d8af70673df0a7982bc103294
yu.hang <yu.hang@nan-mi.cn>
2017-07-07 09:15:17 +0800

    提交生产环境发邮件所需要引入包

--- src/main/java/cn/memedai/orientdb/sns/realtime/service/MailService.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/service/MailService.groovy
@@ -1,6 +1,7 @@
 package cn.memedai.orientdb.sns.realtime.service
 
 import cn.memedai.orientdb.sns.realtime.util.MailUtils
+import org.apache.commons.codec.binary.Base64
 import org.apache.commons.lang.StringUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
