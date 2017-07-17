607d97b230a401ab27d48ac6ebf5a78a227d7e3f
kisho <kisho.dai@mi-me.com>
2017-07-17 13:01:54 +0800

    使用OrientGraphFactory的连接池功能

--- src/main/java/cn/memedai/orientdb/sns/realtime/sql/OrientSql.groovy
+++ src/main/java/cn/memedai/orientdb/sns/realtime/sql/OrientSql.groovy
@@ -1,17 +1,16 @@
 package cn.memedai.orientdb.sns.realtime.sql
 
 import com.orientechnologies.common.concur.ONeedRetryException
-import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
 import com.orientechnologies.orient.core.sql.OCommandSQL
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
 import org.apache.commons.collections.CollectionUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
+import javax.annotation.PostConstruct
 import javax.annotation.Resource
 import java.text.MessageFormat
-import java.util.concurrent.locks.Lock
-import java.util.concurrent.locks.ReentrantLock
 
 /**
  * Created by kisho on 2017/6/8.
@@ -24,26 +23,10 @@ class OrientSql {
     private String checkEdgeSql = 'select from (select expand(out_{0}) from {1}) where in = {2}'
     private String createEdgeSql = 'create edge {0} from {1} to {2}'
 
-    private ThreadLocal<ODatabaseDocumentTx> threadLocal = new ThreadLocal<>()
-
-    private Lock lock = new ReentrantLock()
-
     @Resource
     private Properties orientDbConfig
 
-    private ODatabaseDocumentTx getTx() {
-        if (threadLocal.get() == null) {
-            lock.lock()
-            try {
-                ODatabaseDocumentTx tx = new ODatabaseDocumentTx(orientDbConfig.url)
-                tx.open(orientDbConfig.userName, orientDbConfig.password)
-                threadLocal.set(tx)
-            } finally {
-                lock.unlock()
-            }
-        }
-        threadLocal.get()
-    }
+    private OrientGraphFactory factory
 
     public <RET> RET execute(String sql, Object... args) {
         long start = System.currentTimeMillis()
@@ -52,7 +35,7 @@ class OrientSql {
         int retry = 0
         while (++retry <= 20) {
             try {
-                ret = getTx().command(new OCommandSQL(sql)).execute(args)
+                ret = factory.getDatabase().command(new OCommandSQL(sql)).execute(args)
                 break
             } catch (ONeedRetryException e) {
                 try {
@@ -78,6 +61,13 @@ class OrientSql {
         if (CollectionUtils.isEmpty(execute(MessageFormat.format(checkEdgeSql, edge, fromRid, toRid)))) {
             execute(MessageFormat.format(createEdgeSql, edge, fromRid, toRid))
         }
+    }
+
+    @PostConstruct
+    private void init() {
+        factory = new OrientGraphFactory(orientDbConfig.url, orientDbConfig.userName, orientDbConfig.password)
+        factory.setupPool(Integer.parseInt(orientDbConfig.minPool), Integer.parseInt(orientDbConfig.maxPool))
+        LOG.info('OrientDB connection pool:minPool->{},maxPool->{}', Integer.parseInt(orientDbConfig.minPool), Integer.parseInt(orientDbConfig.maxPool))
     }
 
 }
